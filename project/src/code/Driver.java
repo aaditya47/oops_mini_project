f8f20a294a7dbd0efda2a52a887b39c8b7059f18
Aaditya <uaaditya759@gmail.com>
2019-09-19 19:35:32 +0530

    Created a main driver class and a class for UI
    Co-Authored-By: aaditya47 <aaditya47@users.noreply.github.com>

---
+++ project/src/code/Driver.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		System.out.println("PATROL DEPLOYMENT APP");
+		UI ui=new UI();
+	}
+
+}
---
+++ project/src/code/UI.java
@@ -0,0 +1,8 @@
+package code;
+
+public class UI {
+	public UI()
+	{
+		
+	}
+}
