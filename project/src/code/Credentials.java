4b402eaf4d03f706674854e83711565232f56498
Aaditya <uaaditya759@gmail.com>
2019-09-26 11:31:11 +0530

    Edited the UI class and added a new credentials class

---
+++ project/src/code/Credentials.java
@@ -0,0 +1,15 @@
+package code;
+
+public class Credentials {
+    String username;
+    String password;
+    public Credentials()
+    {
+
+    }
+    public Credentials(String username,String password)
+    {
+        this.username=username;
+        this.password=password;
+    }
+}
--- project/src/code/UI.java
+++ project/src/code/UI.java
@@ -1,10 +1,38 @@
 package code;
+import java.util.*;
 
+import javax.lang.model.util.ElementScanner6;
 public class UI {
 	Credentials credentials;
-	static String sessionID;
+	int sessionID;
+	Scanner s;
 	public UI()
 	{
-		
+		s=new Scanner(System.in);
+		// TODO - Write code to assign random session ID and display it
+		openLoginPage();
+	}
+	public void openLoginPage()
+	{
+		credentials=new Credentials();
+		while(true)
+		{
+			System.out.println("Enter username: ");
+			String username=s.next();
+			System.out.println("Enter password: ");
+			String password=s.next();
+			// TODO - Write code to check username and password
+			if(/*passwords match*/)
+			{
+				System.out.println("Successfully logged in!");
+				credentials.username=username;
+				credentials.password=password;
+				break;
+			}
+			else
+			{
+				System.out.println("Invalid credentials. Please try again.");
+			}
+		}
 	}
 }
