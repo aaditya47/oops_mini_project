13b9ae56635b3efa4c4cc5e491c40ad00b88a9d8
Aaditya <uaaditya759@gmail.com>
2019-10-08 20:33:30 +0530

    Simplified the driver class

--- project/src/code/Driver.java
+++ project/src/code/Driver.java
@@ -1,44 +1,14 @@
 package code;
 
 public class Driver {
-
+	static Scanner s;
 	public static void main(String[] args) {
+		s=new Scanner(System.in);
 		System.out.println("PATROL DEPLOYMENT APP");
 		UI ui=new UI();
-		while(true)
-		{
-			System.out.println("Is there anything else we can help you out with? Type 1 if yes or any other number if no");
-			int opt=new Scanner(System.in).nextInt();
-			if(opt==1)
-			{
-				System.out.println("Type 1 if you want to view the status of a previous complaint, Type 2 if you want to add a payment method");
-				opt=new Scanner(System.in).nextInt();
-				switch(opt)
-				{
-					case 1:
-						System.out.println("Enter username: ");
-						String username=new Scanner(System.in).next();
-						int count;
-						for(int i=0;i<ThreatClassifier.lastIndex;++i)
-						{
-							if(ThreatClassifier.complaintList[i].user.equals(username))
-							{
-								count++;
-								System.out.println("Your "+count+"th pending case is in position "+(i+1)+" and will be taken care of shortly. Please wait");
-							}
-						}
-						break;
-					case 2:
-						// TODO - add code to add payment method
-				}
-			}
-			else
-			{
-				break;
-			}
-		}
-		
-
 	}
-
+	public void main()
+	{
+		
+	}
 }
