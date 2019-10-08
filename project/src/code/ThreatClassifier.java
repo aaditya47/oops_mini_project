c48e5eb14a1310112d814005823b0a874d079f08
Aaditya <uaaditya759@gmail.com>
2019-10-08 20:32:51 +0530

    Updated the classify function

--- project/src/code/ThreatClassifier.java
+++ project/src/code/ThreatClassifier.java
@@ -12,9 +12,31 @@ public class ThreatClassifier {
         }
         lastIndex=0;
     }
-    public void classify()
+    public void classify(long int complaintID)
     {
-        // TODO - insert code to classify
+        for(int i=0;i<1000;++i)
+        {
+            if(complaintID==complaintList[i].complaintID)
+            {
+                classify(complaintList[i].user,i);
+                break;
+            }
+        }
+    }
+    public void classify(Credentials c,int index)
+    {
+        for(int i=0;i<1000;++i)
+        {
+            for(int j=i+1;j<1000;++j)
+            {
+                if((0.35*(CredentialsDatabase.getCrediblity(complaintList[i]))+0.65*complaintList[i].threatLevel)<(0.35*(CredentialsDatabase.getCrediblity(complaintList[j]))+0.65*(complaintList[j].threatLevel)))
+                {
+                    Object temp=complaintList[i];
+                    complaintList[i]=complaintList[j];
+                    complaintList[j]=temp;
+                }
+            }
+        }
     }
     public static void insert(Complaint c)
     {
