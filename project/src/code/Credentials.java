93888ca24735d8d7c13161a590d54a4338125830
Aaditya <uaaditya759@gmail.com>
2019-10-08 20:31:42 +0530

    Added new parameter and deleted default constructor

--- project/src/code/Credentials.java
+++ project/src/code/Credentials.java
@@ -2,14 +2,16 @@ package code;
 
 public class Credentials {
     String username;
+    String aadhaar;
     String password;
     public Credentials()
     {
 
     }
-    public Credentials(String username,String password)
+    public Credentials(String username,String aadhaar,String password)
     {
         this.username=username;
+        this.aadhaar=aadhaar;
         this.password=password;
     }
 }
